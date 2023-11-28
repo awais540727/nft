97fadb0f649282be0e11ce6e5d13ffa78c449a97
Awais Ali <awaisali786378@gmail.com>
2023-11-29 01:26:12 +0500

    About Page Design

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -35,11 +35,40 @@ const About = () => {
         </h2>
         <div className="text-center">
           <video
-            autoPlay muted loop
+            autoPlay
+            muted
+            loop
             style={{ width: "500px", height: "500px" }}
           >
             <source src="/images/about.mp4" type="video/mp4" />
           </video>
+        </div>
+
+        <h2 className="text-center">
+          NFT Gen Marketplace with everything for everyone
+        </h2>
+        <div className="about-card p-1 mt-5 mb-5">
+          <div className="row mt-5 mb-5">
+            <div className="col-lg-5 col-md-5 col-sm-12 col-12 text-center">
+              <img
+                src="/images/about.svg"
+                alt=""
+                style={{ width: "350px", height: "350px" }}
+              />
+            </div>
+            <div className="col-lg-5 col-md-5 col-sm-12 col-12 m-3 tect-center">
+              <h2 className="mt-3 mb3">
+                Create, Sell, Well and collect your best NFTs with us very fast
+              </h2>
+              <p className="mt-3 mb-3">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro
+                qui id, facilis praesentium aperiam ducimus distinctio?
+              </p>
+              <button className="btn btn-navbar mt-3 mb-3">
+                Connect Wallet
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
